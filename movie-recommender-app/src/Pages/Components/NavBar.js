add0facd233355d2eb50a691d25836f6ff1bc27d
Bittu Kumar <134127783+bittukr01@users.noreply.github.com>
2025-11-13 01:13:21 +0530

    Update NavBar.js

--- movie-recommender-app/src/Pages/Components/NavBar.js
+++ movie-recommender-app/src/Pages/Components/NavBar.js
@@ -5,7 +5,7 @@ import Logo from "./images/Logo2.png";
 
 const NavBar = ({ isHome }) => {
     const gitRepoLink =
-        "https://github.com/shohan2001/React-Flask-Movie-Recommendation-App";
+        "https://github.com/bittukr01/MovieRecomendation.git";
     return (
         <div className="container header">
             <Link to="/ ">
